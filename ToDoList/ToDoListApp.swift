7220445018efcbe8a307a168e4e2174d1897250c
SEKI-YUTA <zard.since1991@gmail.com>
2023-07-04 22:30:03 +0900

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by 関佑太 on 2023/07/04.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
